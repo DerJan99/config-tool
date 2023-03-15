904789e41856344dd46c2e8fdd81b3d6c55406f3
Jan von Sondern <jan.vonsondern@antrago.de>
2023-03-15 09:01:48 +0100

    feat(ui-common): added help-tooltip to ui-common library

---
+++ libs/ui/common/src/lib/help-tooltip/help-tooltip.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HelpTooltipComponent } from './help-tooltip.component';
+
+describe('HelpTooltipComponent', () => {
+  let component: HelpTooltipComponent;
+  let fixture: ComponentFixture<HelpTooltipComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HelpTooltipComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HelpTooltipComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
