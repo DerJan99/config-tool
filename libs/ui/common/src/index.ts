3112f5f472fdce3658cd92c0a042bf3109a0d163
Jan von Sondern <jan.vonsondern@antrago.de>
2023-03-15 10:38:23 +0100

    ci(azure-pipelines): added new pipeline to project

--- libs/ui/common/src/index.ts
+++ libs/ui/common/src/index.ts
@@ -1,7 +1,15 @@
 export * from './lib/ui-common.module';
 
-export * from './lib/sidenav/sidenav.component';
+export * from './lib/basic-button/basic-button.component';
+
+export * from './lib/expansion-panel/expansion-panel.component';
+
+export * from './lib/help-tooltip/help-tooltip.component';
+
+export * from './lib/icon-button/icon-button.component';
 
 export * from './lib/raised-button/raised-button.component';
 
-export * from './lib/help-tooltip/help-tooltip.component';
+export * from './lib/sidenav/sidenav.component';
+
+export * from './lib/stroked-button/stroked-button.component';
