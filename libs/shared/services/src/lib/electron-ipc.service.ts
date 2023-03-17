c93d3557b21e0c1b7abdf1eb4b0c7b3605cf6b3e
Jan von Sondern <jan.vonsondern@antrago.de>
2023-03-17 09:11:02 +0100

    refactor(electron-ipc.service): refactored methods inside ElectronIpcService

--- libs/shared/services/src/lib/electron-ipc.service.ts
+++ libs/shared/services/src/lib/electron-ipc.service.ts
@@ -7,7 +7,19 @@ import { ElectronService } from 'ngx-electron';
 export class ElectronIpcService {
   public electronService = new ElectronService().ipcRenderer;
 
+  private send(channel: string, ...args: any): void {
+    if (this.electronService) {
+      this.electronService.send(channel, ...args);
+    }
+  }
+
+  private listen(channel: string, listener: (...args: any) => void): void {
+    if (this.electronService) {
+      this.electronService.on(channel, listener);
+    }
+  }
+
   public toggleDevTools(state: boolean): void {
-    this.electronService.send('dev-tools', state);
+    this.send('dev-tools', state);
   }
 }
